96c6bc9071a1e47cf2f5d5399a1b0cdc02cade8c
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-08-07 18:56:32 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
 
+var Name = prompt("Enter your name  :")
+var Mark = prompt("Hello " + Name + " ! \nEnter your mark please:")
+var MarkFloor = Math.floor(Mark)
+
+
+    if(MarkFloor >= 91 && MarkFloor <= 100 ){
+        alert("Your grade is A+ . congratulations you Pass ")
+    }
+    else if(MarkFloor >= 51 && MarkFloor <= 60 ){
+        alert("Your grade is D . congratulations you Pass")
+    }
+    else if(MarkFloor >= 61 && MarkFloor <= 70 ){
+        alert("Your grade is C . congratulations you Pass")
+    }
+    else if(MarkFloor >= 71 && MarkFloor <= 80 ){
+        alert("Your grade is B . congratulations you Pass")
+    }
+    else if(MarkFloor >= 81 && MarkFloor <= 90 ){
+        alert("Your grade is A . congratulations you Pass")
+    }
+    else {
+   alert("Your grade is F . sorry you Failed")
+     
+    }
+        
